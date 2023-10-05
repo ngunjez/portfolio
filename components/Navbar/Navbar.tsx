8cb81a631f77fda1a9a0b5ae2ab43a0f154cc8ee
ngunjiri <owen2ngunjiri@gmail.com>
2023-10-05 17:35:51 +0300

    first commit

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import { NavbarContainer } from '@/styles/Navbar/Navbar';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <NavbarContainer>
+        
+    </NavbarContainer>
+  )
+}
+
+export default Navbar;
---
+++ styles/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const NavbarContainer = styled.nav`
+position:fixed;
+width:100vw;
+height:20vh;
+background-color:#000;
+`
