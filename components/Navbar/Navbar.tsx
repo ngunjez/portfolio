d4422ad8a4d432a4a52a3649d1c7fbdd08e4169c
ngunjiri <owen2ngunjiri@gmail.com>
2023-10-18 21:01:10 +0300

    updates

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,10 +1,18 @@
-import { NavbarContainer } from '@/styles/Navbar/Navbar';
+import { NavLinks, NavLogo, NavbarContainer, Navlink } from '@/styles/Navbar/Navbar';
+import Link from 'next/link'; 
 import React from 'react';
 
 const Navbar = () => {
-  return (
+  return ( 
     <NavbarContainer>
-        
+      <NavLogo></NavLogo>
+        <NavLinks>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+          <Link href="/resume">Resume</Link>
+          <Link href="/projects">Projects</Link>
+        </NavLinks>
     </NavbarContainer>
   )
 }
--- styles/Navbar/Navbar.tsx
+++ styles/Navbar/Navbar.tsx
@@ -3,6 +3,47 @@ import styled from "styled-components";
 export const NavbarContainer = styled.nav`
 position:fixed;
 width:100vw;
-height:20vh;
+height:15vh;
 background-color:#000;
+display:flex;
+justify-content: space-between;  /* Use justify-content to space items */
+  padding: 0 50px;  /* Add padding to space items away from edges */
+  align-items: center;  /* Align items vertically in the center */
+`
+
+export const NavLogo = styled.div`
+position:absolute;
+height:150px;
+width:405px;
+display:flex;
+background-image:url("/Logo.jpg");
+background-position:center;
+background-repeat:no-repeat;
+background-size:cover;
+cursor:pointer;
+`
+
+export const NavLinks = styled.div`
+position:absolute;
+display:flex;
+flex-direction:row;
+// background-color:red;
+width:483.47px;
+height:auto;
+padding-left:30px;
+justify-content:space-between;
+align-items:center;
+color:#fff;
+right:40px;
+font-size: clamp(20px, calc(1.25rem + ((1vw - 7.68px) * 0.3472)), 24px);
+
+`
+
+export const Navlink = styled.div`
+text-decoration:none;
+font-weight:bold;
+cursor:pointer;
+font-family:Inter;
+
+
 `
